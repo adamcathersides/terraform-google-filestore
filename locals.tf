2107e1054c9b22ff19c582dcba2b76459abba9ab
Adam Cathersides <adamcathersides@gmail.com>
2025-07-16 15:22:21 +0000

    Remove count for client and project config

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  project_id     = var.enable_auto_backup ? data.google_client_config.current[0].project : null
-  project_number = var.enable_auto_backup ? data.google_project.current[0].number : null
-  region         = var.enable_auto_backup ? data.google_client_config.current[0].region : null
+  project_id     = var.enable_auto_backup ? data.google_client_config.current.project : null
+  project_number = var.enable_auto_backup ? data.google_project.current.number : null
+  region         = var.enable_auto_backup ? data.google_client_config.current.region : null
 }
